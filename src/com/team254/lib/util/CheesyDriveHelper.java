e7782411c7d6142c0b22fb4d53b05edc2806b73c
johuang50 <35316075+johuang50@users.noreply.github.com>
2019-02-26 22:29:12 -0500

    Update CheesyDriveHelper.java

--- src/com/team254/lib/util/CheesyDriveHelper.java
+++ src/com/team254/lib/util/CheesyDriveHelper.java
@@ -23,7 +23,7 @@ public class CheesyDriveHelper {
     private static final double kLowNegInertiaFarScalar = 5.0;
 
     private static final double kHighSensitivity = 0.95;
-    private static final double kLowSensitiity = 1.3;
+    private static final double kLowSensitiity = 1.3;//sensitivity
 
     private static final double kQuickStopDeadband = 0.2;
     private static final double kQuickStopWeight = 0.1;
